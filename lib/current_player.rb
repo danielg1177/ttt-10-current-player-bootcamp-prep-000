aa8ffcbed4dde2ec0c603a438c9aaa64c4a142ee
dauntless-strategy-1036 <danielgordon19972@gmail.com>
2021-02-10 17:48:50 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,8 +2,13 @@ def turn_count(board)
   board.each do |spot|
     counter = 0
     if spot == "X" || spot == "O"
-      counter++
+      counter += 1
     end
   end
   counter
+end
+
+def current_player(board)
+  if turn_count(board) / 2 % 0
+    
 end
